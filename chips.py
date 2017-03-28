e05a12fbe8e128a219b175859890c82fdb6a0aff
amirshams <amir.shams84@gmail.com>
2017-03-28 11:49:11 -0400

    Add files via upload

---
+++ chips.py
@@ -0,0 +1,482 @@
+# ################################### INFO ##################################### #
+# ChIp-seq analysis workflow 1.0
+# Author: Amir Shams
+# Email: amir.shams84@gmail.com
+# ################################### IMPORTED LIBRARY ######################### #
+
+
+import sys
+import os
+import argparse
+import time
+import multiprocessing
+import platform
+import logging as log
+import itertools
+import errno
+import signal
+import datetime
+import traceback
+import subprocess
+# ################################### GLOBAL VARIABLES ######################## #
+
+
+CHECK_MARK = "OK"
+FAILED_MARK = ":("
+DEFAULT_OUTPUTDIR = "/ChIps_OUTPUTDIR/"
+DEFAULT_TESTDIR = "/ChIps_TESTDIR/"
+DEFAULT_EXECDIR = "/ChIps_EXECDIR/"
+DEFAULT_PROCESSORS = str(multiprocessing.cpu_count())
+DEFAULT_PREFIX = "ChIps"
+# ################################### EXECUTIONS ############################### #
+
+
+def execute_functions(function_name, processors, outputdir, thread_type, func_mode, *args):
+	list_of_pid = []
+	list_of_stderr = []
+	list_of_stdout = []
+	if thread_type == 'fork':
+		threads = int(processors) + 1
+		processors = '1'
+	elif thread_type == 'multi':
+		threads = 2
+	for thread in range(1, threads):
+		pid_file = 'run.pid' + str(thread)
+		stderr_file = 'stderr' + str(thread)
+		stdout_file = 'stdout' + str(thread)
+		run_pid = outputdir + pid_file
+		stderr = outputdir + stderr_file
+		stdout = outputdir + stdout_file
+		flag = function_name(processors, outputdir, stderr, stdout, run_pid, *args)
+		if flag is False:
+			sys.exit(2)
+		list_of_pid.append(pid_file)
+		list_of_stderr.append(stderr_file)
+		list_of_stdout.append(stdout_file)
+	flag, stderr = process_monitor(list_of_pid, list_of_stderr, list_of_stdout, outputdir, threads, func_mode)
+	if flag is False:
+		print "Process monitor failed."
+	else:
+		pass
+	return (True, stderr)
+
+
+def process_monitor(pid_list, stderr_list, stdout_list, outputdir, threads, mode):
+
+	finished_flag = False
+	flag_list = {}
+	for each_pid in pid_list:
+		flag_list[each_pid] = False
+	toolbar_width = threads
+	sys.stdout.write("[%s]" % (" " * toolbar_width))
+	sys.stdout.flush()
+	sys.stdout.write("\b" * (toolbar_width + 1))  # return to start of line, after '['
+	while finished_flag is False:
+		for pid_file, stderr_file, stdout_file in itertools.izip(pid_list, stderr_list, stdout_list):
+			f = open(outputdir + pid_file)
+			the_pid = int(f.read().rstrip())
+			if pid_exists(the_pid) is False:
+				sys.stdout.write("OK")
+				sys.stdout.flush()
+				flag_list[pid_file] = True
+				flag, stderr = validate_execution(stderr_file, stdout_file, outputdir, mode)
+				if flag is False:
+					sys.stdout.write(":(")
+					report("[:()]")
+					print "Error in result of this thread: ", str(the_pid)
+					error("Error in result of this thread: " + str(the_pid))
+					print "All generated threads killed."
+					error("All generated threads killed.")
+					kill_pid_list(pid_list, outputdir)
+					#print stderr
+					print "ABORTING!!!"
+					report("ABORTING!!!")
+					sys.exit(2)
+			if False in flag_list.values():
+				finished_flag = False
+			else:
+				finished_flag = True
+		time.sleep(1)
+	sys.stdout.write("\n")
+	report("[OK]")
+	return (True, stderr)
+
+
+def pid_exists(pid):
+	if pid < 0:
+		return False
+	if pid == 0:
+		# According to "man 2 kill" PID 0 refers to every process
+		# in the process group of the calling process.
+		# On certain systems 0 is a valid PID but we have no way
+		# to know that in a portable fashion.
+		raise ValueError('invalid PID 0')
+	try:
+		os.kill(pid, 0)
+	except OSError as err:
+		if err.errno == errno.ESRCH:
+			# ESRCH == No such process
+			return False
+		elif err.errno == errno.EPERM:
+			# EPERM clearly means there's a process to deny access to
+			return True
+		else:
+			# According to "man 2 kill" possible error values are
+			# (EINVAL, EPERM, ESRCH)
+			raise
+	else:
+		return True
+
+
+def validate_execution(stderr, stdout, outputdir, mode):
+	if mode == 'usearch':
+		f = open(outputdir + stdout, 'rU')
+		data = f.read()
+		#print data
+		if '---Fatal error---' in data or 'Invalid command line' in data:
+			return (False, data)
+		else:
+			return(True, data)
+	elif mode == 'mothur':
+		f = open(outputdir + stdout, 'rU')
+		data = f.read()
+		if '[ERROR]:' in data:
+			return (False, data)
+		else:
+			return(True, data)
+	elif mode == 'mafft':
+		f = open(outputdir + stdout, 'rU')
+		data = f.read()
+		if 'No such file or directory' in data or 'cannot open file' in data:
+			return (False, data)
+		else:
+			return(True, data)
+
+
+def kill_pid_list(pid_list, outputdir):
+	for pid in pid_list:
+		f = open(outputdir + pid)
+		the_pid = int(f.read().rstrip())
+		try:
+			os.kill(the_pid, signal.SIGTERM)
+		except OSError:
+			pass
+		print the_pid, "Killed!"
+	return True
+
+
+def execute(command, ** kwargs):
+	assert isinstance(command, list), "Expected 'command' parameter to be a list containing the process/arguments to execute. Got %s of type %s instead" % (command, type(command))
+	assert len(command) > 0, "Received empty list of parameters"
+	retval = {
+			"exitCode": -1,
+			"stderr": u"",
+			"stdout": u"",
+			"execTime": datetime.timedelta(0),
+			"command": None,
+			"pid": None
+		}
+	retval["command"] = command
+	log.info("::singleProcessExecuter > At %s, executing \"%s\"" % (datetime.datetime.now(), " ".join(command)))
+	# print("::singleProcessExecuter > At %s, executing \"%s\"" % (datetime.datetime.now(), " ".join(parameter)))
+	cwd = kwargs.get("cwd", os.getcwd())
+	sheel = kwargs.get("shell", True)
+	startDatetime = datetime.datetime.now()
+	myPopy = subprocess.Popen(command, cwd=cwd, preexec_fn=os.seteuid(os.getuid()), shell=sheel, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+	retval["pid"] = myPopy.pid
+	log.debug("::singleProcessExecuter > Command \"%s\" got pid %s" % (" ".join(command), myPopy.pid))
+	try:
+		retval["stdout"], retval["stderr"] = myPopy.communicate()
+		myPopy.wait()
+	except OSError, osErr:
+		log.debug("::singleProcessExecuter > Got %s %s in myPopy.communicate() when trying get output of command %s. It is probably a bug (more info: http://bugs.python.org/issue1731717)" % (osErr, type(osErr), command[0]))
+	except Exception, e:
+		log.warn("::singleProcessExecuter > Got %s %s when trying to get stdout/stderr outputs of %s" % (type(e), e, " ".join(command)))
+		log.debug("::singleProcessExecuter > Got %s %s when trying to get stdout/stderr outputs of %s. Showing traceback:\n%s" % (type(e), e, " ".join(command), traceback.format_exc()))
+		raise
+	retval["exitCode"] = myPopy.returncode
+	retval["execTime"] = datetime.datetime.now() - startDatetime
+	return retval
+# ################################### UTILITIES ############################### #
+
+
+def check_it_and_remove_it(filename, noreport=False):
+	try:
+		os.remove(filename)
+		if noreport is False:
+			pass
+	except OSError:
+		pass
+
+
+def isFileExist(fname):
+	# check the exitence/access/path of a file
+	if os.path.isfile(fname) and os.path.exists(fname) and os.access(fname, os.R_OK):
+		return True
+	else:
+		return False
+# ################################### LOGGING & REPORTING ##################### #
+
+
+def error(report_string):
+	f = open(report_file, "a")
+	f.write('###############################  ERROR   ###################################\n')
+	f.write(report_string)
+	f.write("\n")
+	f.write('############################################################################\n')
+	f.close()
+
+
+def report(report_string):
+	f = open(report_file, "a")
+	f.write(report_string.encode('utf8'))
+	f.write("\n")
+	f.close()
+# ################################### OBJECTS ################################## #
+
+
+class mothur_process:
+	def __init__(self, mothur_input_dictionary):
+		for var_name, var_value in mothur_input_dictionary.items():
+			setattr(self, var_name, var_value)
+
+	def build_mothur_command(self):
+		space = " "
+		string = ''
+		if hasattr(self, 'nohup_in'):
+			string += self.nohup_in + space
+		string += self.mothur_exec_path + space + '"#set.dir(output=' + self.outputdir + ');' + space
+		string += 'set.logfile(name=mothur.' + self.command + '.logfile);' + space
+		string += 'set.current(processors=' + self.processors + ');' + space
+		string += self.command + '('
+		if hasattr(self, 'parameters') and len(self.parameters) > 0:
+			for each_element in self.parameters:
+				string += each_element
+		string += ');"'
+		if hasattr(self, 'nohup_out'):
+			string += space + self.nohup_out
+		if hasattr(self, 'pid_file'):
+			string += ' echo $! > ' + self.pid_file
+		report(string)
+		print string
+		return string
+
+	def execute_mothur_command(self):
+		exec_dict = {}
+		exec_dict = self.execute([self.build_mothur_command()])
+		if exec_dict['exitCode'] != 0:
+			print "ERROR occurred!!!!"
+			return (False, exec_dict)
+		else:
+			return(True, exec_dict)
+
+	def execute(self, command, ** kwargs):
+		assert isinstance(command, list), "Expected 'command' parameter to be a list containing the process/arguments to execute. Got %s of type %s instead" % (command, type(command))
+		assert len(command) > 0, "Received empty list of parameters"
+		retval = {
+					"exitCode": -1,
+					"stderr": u"",
+					"stdout": u"",
+					"execTime": datetime.timedelta(0),
+					"command": None,
+					"pid": None
+				}
+		retval["command"] = command
+		log.info("::singleProcessExecuter > At %s, executing \"%s\"" % (datetime.datetime.now(), " ".join(command)))
+		cwd = kwargs.get("cwd", os.getcwd())
+		#user = kwargs.get("user", os.getuid)
+		sheel = kwargs.get("shell", True)
+		startDatetime = datetime.datetime.now()
+		myPopy = subprocess.Popen(command, cwd=cwd, preexec_fn=os.seteuid(os.getuid()), shell=sheel, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+		retval["pid"] = myPopy.pid
+		log.debug("::singleProcessExecuter > Command \"%s\" got pid %s" % (" ".join(command), myPopy.pid))
+		try:
+			retval["stdout"], retval["stderr"] = myPopy.communicate()
+			myPopy.wait()
+		except OSError, osErr:
+			log.debug("::singleProcessExecuter > Got %s %s in myPopy.communicate() when trying get output of command %s. It is probably a bug (more info: http://bugs.python.org/issue1731717)" % (osErr, type(osErr), command[0]))
+		except Exception, e:
+			log.warn("::singleProcessExecuter > Got %s %s when trying to get stdout/stderr outputs of %s" % (type(e), e, " ".join(command)))
+			log.debug("::singleProcessExecuter > Got %s %s when trying to get stdout/stderr outputs of %s. Showing traceback:\n%s" % (type(e), e, " ".join(command), traceback.format_exc()))
+			raise
+		retval["exitCode"] = myPopy.returncode
+		retval["execTime"] = datetime.datetime.now() - startDatetime
+		return retval
+# ################################### MOTHUR FUNCTIONS ######################## #
+
+
+def test_mothur(processors, outputdir, stderr, stdout, run_pid, mothur_exec_PATH):
+	# test mothur to see if it is working and grab the version of mothur by scanning its output log
+	mothur_input_dictionary = {}
+	command = 'get.current'
+	mothur_input_dictionary['command'] = command
+	mothur_input_dictionary['mothur_exec_path'] = mothur_exec_PATH
+	mothur_input_dictionary['processors'] = processors
+	mothur_input_dictionary['outputdir'] = outputdir
+	mothur_input_dictionary['nohup_in'] = 'nohup'
+	mothur_input_dictionary['nohup_out'] = '> ' + stdout + ' 2> ' + stderr + ' & echo $! > ' + run_pid
+	#parameter_list = []
+	make_file_object = mothur_process(mothur_input_dictionary)
+	exec_dict = {}
+	flag, exec_dict = make_file_object.execute_mothur_command()
+	if flag is False:
+		print "[FATAL-ERROR]: Commandline can not be executed!!!"
+		print "ABORTING!!!"
+		sys.exit(2)
+	return True
+
+
+def parse_mothur_logfile(logfile, keyword, output_container, mode=None):
+	if mode == 'file':
+		f = open(logfile, 'rU')
+		data = f.read().split('\n')
+	else:
+		data = logfile
+	for line in data.split('\n'):
+		if keyword in line:
+			output_container.append(line)
+	if len(output_container) < 1:
+		return False
+	return True
+# ################################### MAIN FUNCTION ########################### #
+
+
+def main(argv):
+	report_string = ''
+	# ++++++++++++++++++++++++++++++ PARSE INPUT ARGUMENTS
+	parser = argparse.ArgumentParser()
+	main_file = parser.add_argument_group('Main file parameters')
+	main_file.add_argument("--biom", help="Universal microbiome abundance matrix file(http://biom-format.org)", action='store')
+	main_file.add_argument("--design", help="Design file: Tab delimited file to assign samples to a specific treatments, or other categories.", action='store')
+	args = parser.parse_args()
+	# ------------------------------ END OF PARSE INPUT ARGUMENTS
+
+	# ++++++++++++++++++++++++++++++ BEURACRATICS PROCEDURES
+	report_string += "######################################################################################################################################\n"
+	print "######################################################################################################################################"
+	report_string += "ChIps 1.0 EXECUTION HAS INITIATED" + '\n'
+	print "ChIps 1.0 EXECUTION HAS INITIATED"
+	report_string += "Initiation time: " + time.strftime("%Y-%m-%d %H:%M:%S") + '\n'
+	print "Initiation time: ", time.strftime("%Y-%m-%d %H:%M:%S")
+	report_string += "###################################################################" + '\n'
+	print "###################################################################"
+	report_string += "INFORMATION ABOUT THE ENVIRONMENT, EXECUTABLES AND PROVIDED DATA" + '\n'
+	print "INFORMATION ABOUT THE ENVIRONMENT, EXECUTABLES AND PROVIDED DATA"
+	report_string += "###################################################################" + '\n'
+	print "###################################################################"
+	report_string += "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" + '\n'
+	print "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+	print "COMMAND LINE:"
+	report_string += "COMMAND LINE:\n"
+	report_string += "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" + '\n'
+	print "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+	commandline_string = 'python ' + ' '.join(sys.argv) + '\n'
+	print commandline_string
+	report_string += commandline_string + '\n'
+	report_string += "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" + '\n'
+	print "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+	report_string += "ARGUMENTS:" + '\n'
+	print "ARGUMENTS:"
+	report_string += "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" + '\n'
+	print "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+
+	# ++++++++++++++++++++++++++++++ OUTPUT DIRECTORY CHECKING
+	args.outputdir = DEFAULT_OUTPUTDIR
+	global report_file
+	report_file = args.outputdir + "ChIps_report.txt"
+	check_it_and_remove_it(report_file, True)
+	report(report_string)
+	# ------------------------------ END OF OUTPUT DIRECTORY CHECKING
+
+	# ++++++++++++++++++++++++++++++ PROCESSORS CHECKING
+	args.processors = DEFAULT_PROCESSORS
+	# ------------------------------ END OF PROCESSORS CHECKING
+
+	# ++++++++++++++++++++++++++++++ PREFIX NAME CHECKING
+	args.prefix = DEFAULT_PREFIX
+	# ------------------------------ END OF PREFIX NAME CHECKING
+
+	# ++++++++++++++++++++++++++++++ EXECUTIVE DIRECTORY CHECKING
+	args.execdir = DEFAULT_EXECDIR
+	# ------------------------------ END OF EXECUTIVE DIRECTORY CHECKING
+
+	# ++++++++++++++++++++++++++++++ CHECKING EXECUTIVES
+	print "\n###################################################################"
+	report("\n###################################################################")
+	print "VERIFYING THE SANITY/VERSION OF EXECUTABLES IN EXECUTIVE DIRECTORY[--execdir]"
+	report("VERIFYING THE SANITY/VERSION OF EXECUTABLES IN EXECUTIVE DIRECTORY[--execdir]")
+	print "###################################################################\n"
+	report("###################################################################\n")
+	print "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+	report("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+	report("0: ENVIRONMENT")
+	print "0: ENVIRONMENT"
+	print "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+	report("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+	print "Operating System version is: ", platform.platform()
+	report("Operating System version is: " + platform.platform())
+	python_version = sys.version.split(' (')[0]
+	print "Python version is: ", python_version
+	report("Python version is: " + python_version)
+	if float(python_version[0:3]) < 2.7:
+		error("Python version is older than 2.7")
+		print "Python version is older than 2.7"
+		print "ABORTING!!!"
+		error("ABORTING!!!")
+		sys.exit(2)
+	print "Number of available CPU core: ", DEFAULT_PROCESSORS
+	report("Number of available CPU core: " + DEFAULT_PROCESSORS)
+	print "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+	report("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+	report("1: MOTHUR")
+	print "1: MOTHUR"
+	print "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+	report("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+	mothur_exec_PATH = args.execdir + 'mothur'
+	if isFileExist(mothur_exec_PATH) is False:
+		error("Your mothur file path has Access/Exist issue")
+		print "Your mothur file path has Access/Exist issue"
+		print "ABORTING!!!"
+		error("ABORTING!!!")
+		sys.exit(2)
+	else:
+		print "mothur execution file path is: ", mothur_exec_PATH
+		report("mothur execution file path is: " + mothur_exec_PATH)
+		#report("Testing mothur executables: ")
+		print "Testing mothur executable: "
+		report("Testing mothur executable: ")
+		flag, stderr = execute_functions(test_mothur, args.processors, args.outputdir, 'multi', 'mothur', mothur_exec_PATH)
+		if flag is False:
+			error("[" + FAILED_MARK + "]")
+			print "[", FAILED_MARK, "]"
+			print "Execution of mothur failed!!!"
+			error("Execution of mothur failed!!!")
+			print "ABORTING!!!"
+			error("ABORTING!!!")
+			sys.exit(2)
+		else:
+			target_lines = []
+			flag = parse_mothur_logfile(stderr, 'mothur v.', target_lines)
+			if flag is False:
+				print "This keyword is not avalaible: mothur v."
+				error("This keyword is not avalaible: mothur v.")
+				print "ABORTING!!!"
+				error("ABORTING!!!")
+				sys.exit(2)
+			report("Mothur executables responding successfully!!!")
+			print "Mothur executables responding successfully!!!"
+			report("version of mothur executables: " + target_lines[0])
+			print "version of mothur executables:", target_lines[0]
+	print "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+	report("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+	# ------------------------------ END OF CHECKING EXECUTIVES
+	# ------------------------------ END OF BEURACRATICS PROCEDURES
+
+	# +++++++++++++++++++++++++++++ FINALIZING
+	print "ChIps EXECUTION COMPLETED."
+	report("ChIps EXECUTION COMPLETED.")
+	report("Completion time: " + time.strftime("%Y-%m-%d %H:%M:%S"))
+	print "Completion time: ", time.strftime("%Y-%m-%d %H:%M:%S")
+	# ----------------------------- END OF FINALIZING
+# ################################### FINITO ################################# #
+if __name__ == "__main__": main(sys.argv[1:])
